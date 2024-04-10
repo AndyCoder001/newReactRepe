e68af71ed2e1e1a50679f838497f079e6d48cbf5
NISHANT DALAL <entrocomedy@gmail.com>
2024-04-10 19:34:52 +0530

    New React App

---
+++ src/MyComponents/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <>
+     <div className="navbar">
+        <a className="active" href="">Home</a>
+        <a href="">Blogs</a>
+        <a href="">Contact</a>
+     </div>
+    </>
+  );
+}
